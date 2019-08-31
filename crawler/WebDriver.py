88c82fa26782d512e259f35e6e7266e1d7e1d655
21jun <21jun7654@gmail.com>
2019-09-01 03:16:07 +0900

    make cycle

--- crawler/WebDriver.py
+++ crawler/WebDriver.py
@@ -16,52 +16,78 @@ class HeadLessChrome:
         self.driver = webdriver.Chrome(self.driver_path, chrome_options=options)
         self.driver.implicitly_wait(1)
 
-    def start(self) -> bool:
-        """
+    # core
 
-        :return: success
-        """
-        # page 접근
-        self.driver.get('https://portal.sejong.ac.kr/jsp/login/uisloginSSL.jsp')
+    def _click_xpath_element(self, xpath):
+        self.driver.find_element_by_xpath(xpath).click()
+
+    def _send_keys_xpath_element(self, xpath, keys):
+        self.driver.find_element_by_xpath(xpath).send_keys(keys)
+
+    def _switch_frame_to(self, xpath, is_parent=None) -> None:
+        if is_parent:
+            self.driver.switch_to.parent_frame()
+            return
+        self.driver.switch_to.frame(self.driver.find_element_by_xpath(xpath))
+
+    # logic
+
+    def _get_page_from_url(self, url):
+        self.driver.get(url)
         self.driver.implicitly_wait(1)
 
-        # 로그인
-        self.driver.find_element_by_xpath('// *[ @ id = "id"]').send_keys(config.id)
-        self.driver.find_element_by_xpath('// *[ @ id = "password"]').send_keys(config.pw)
-        self.driver.find_element_by_xpath('// *[ @ id = "logbtn"]').click()
+    def _login(self, id, pw):
+        self._send_keys_xpath_element('// *[ @ id = "id"]', id)
+        self._send_keys_xpath_element('// *[ @ id = "password"]', pw)
+        self._click_xpath_element('// *[ @ id = "logbtn"]')
+        self.driver.implicitly_wait(1)
 
+    def _logic_go_to_riaframe(self):
+        # 컨텐츠 frame 으로 이동
+        self._switch_frame_to('// *[ @ id = "contentFrm"]')
+        # 수강신청내역 frame 으로 이동
+        self._switch_frame_to('// *[ @ id = "riaframe"]')
+
+    def _logic_move_to_sugang(self):
         # 매뉴 frame 으로 switch
-        self.driver.implicitly_wait(1)
-        self.driver.switch_to.frame(self.driver.find_element_by_xpath('// *[ @ id = "menuFrm"]'))
+        self._switch_frame_to('// *[ @ id = "menuFrm"]')
         # 수업/성적 클릭
-        self.driver.find_element_by_xpath(
-            '//*[@id="mainTable"]/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr[3]').click()
+        self._click_xpath_element('//*[@id="mainTable"]/tbody/tr[1]/td/table/tbody/tr/td/table/tbody/tr[3]')
         # 강좌조회 및 수강신청 클릭
-        self.driver.find_element_by_xpath('// *[ @ id = "SELF_STUDSELF_SUB_30"] / table / tbody / tr[1] / td').click()
+        self._click_xpath_element('// *[ @ id = "SELF_STUDSELF_SUB_30"] / table / tbody / tr[1] / td')
         # 수강내용조회/출력 클릭
-        self.driver.find_element_by_xpath(
-            '// *[ @ id = "SELF_STUDSELF_SUB_30SELF_MENU_10"] / table / tbody / tr[5]').click()
+        self._click_xpath_element('// *[ @ id = "SELF_STUDSELF_SUB_30SELF_MENU_10"] / table / tbody / tr[5]')
 
         # 수강내용조회/출력 컨텐츠 frame 으로 switch
         # https://stackoverflow.com/questions/17856187/how-to-navigate-back-to-current-page-from-frame-in-selenium-webdriver
         self.driver.implicitly_wait(1)
-        self.driver.switch_to.parent_frame()  # 부모 frame 으로 이동
-        self.driver.switch_to.frame(self.driver.find_element_by_xpath('// *[ @ id = "contentFrm"]'))  # 컨텐츠 frame 으로 이동
-        self.driver.switch_to.frame(self.driver.find_element_by_xpath('// *[ @ id = "riaframe"]'))  # 수강신청내역 frame 으로 이동
-        # 의견등록 클릭
+        # 부모 frame 으로 이동
+        self._switch_frame_to(None, is_parent=True)
+        # 수강신청내역 frame 으로 이동
+        self._logic_go_to_riaframe()
+
+    def _logic_cycle_01(self):
+        """
+        수강내역에 의견등록을 수행하는 사이클
+        window 는 사이클 함수내에서 관리한다. (self 로 멤버변수로 저장하지 않음)
+        한번 사이클이 돌면 바로 다음 사이클이 돌 수 있도록 frame 변경함
+        :return:
+        """
+
+        # 메인(uis) 페이지 윈도우 저장
         main_window = self.driver.window_handles[0]
-        self.driver.find_element_by_xpath(
-            '// *[ @ id = "rpt"] / tbody / tr[2] / td / div / div[1] / table / tbody / tr[2] / td[9] / button').click()
+        # 의견등록 클릭
+        self._click_xpath_element(
+            '// *[ @ id = "rpt"] / tbody / tr[2] / td / div / div[1] / table / tbody / tr[2] / td[9] / button')
 
         # 의견등록 window 로 switch
         # https://stackoverflow.com/questions/10629815/how-to-switch-to-new-window-in-selenium-for-python
         new_window = self.driver.window_handles[1]
         self.driver.switch_to.window(new_window)
-        self.driver.find_element_by_xpath('//*[@id="guide"]').click()
-        self.driver.find_element_by_xpath('//*[@id="container"]/div/div/div[2]/div/div[2]/a/span/img').click()
-        self.driver.find_element_by_xpath('//*[@id="guide"]').click()
-        self.driver.find_element_by_xpath(
-            '//*[@id="tx_editor_form"]/div/table/tbody/tr[1]/td/div/div/ul/li/input').send_keys('제목')
+        self._click_xpath_element('//*[@id="guide"]')
+        self._click_xpath_element('//*[@id="container"]/div/div/div[2]/div/div[2]/a/span/img')
+        self._click_xpath_element('//*[@id="guide"]')
+        self._send_keys_xpath_element('//*[@id="tx_editor_form"]/div/table/tbody/tr[1]/td/div/div/ul/li/input', '제목')
         # 저장(submit)은 구현하지 않음.
 
         # 메인 window 로 switch
@@ -71,20 +97,29 @@ class HeadLessChrome:
         self.driver.close()
         self.driver.switch_to.window(main_window)
         # print(self.driver.page_source)
-        # 다시 메인 window 로 넘어오면 default 로 switch 되어있다.
+        # 다시 메인 window 로 넘어오면 default frame 으로 switch 되어있다.
         # 다시 원하는 frame 으로 switch 해야함
 
-        # 반복 작업 (Loop 가능)
-        self.driver.switch_to.frame(self.driver.find_element_by_xpath('// *[ @ id = "contentFrm"]'))  # 컨텐츠 frame 으로 이동
-        self.driver.switch_to.frame(self.driver.find_element_by_xpath('// *[ @ id = "riaframe"]'))  # 수강신청내역 frame 으로 이동
-        self.driver.find_element_by_xpath(
-            '// *[ @ id = "rpt"] / tbody / tr[2] / td / div / div[1] / table / tbody / tr[2] / td[9] / button').click()
-        new_window = self.driver.window_handles[1]
-        self.driver.switch_to.window(new_window)
-        self.driver.find_element_by_xpath('//*[@id="guide"]').click()
-        self.driver.find_element_by_xpath('//*[@id="container"]/div/div/div[2]/div/div[2]/a/span/img').click()
-        self.driver.find_element_by_xpath('//*[@id="guide"]').click()
-        self.driver.find_element_by_xpath(
-            '//*[@id="tx_editor_form"]/div/table/tbody/tr[1]/td/div/div/ul/li/input').send_keys('제목')
+        # 수강신청내역 frame 으로 이동
+        self._logic_go_to_riaframe()
+
+    # entry
+
+    def start(self) -> bool:
+        # page 접근
+        self._get_page_from_url('https://portal.sejong.ac.kr/jsp/login/uisloginSSL.jsp')
+
+        # 로그인
+        self._login(config.id, config.pw)
+
+        # 수강신청 사이클은 이 아래 코드를 변경
+
+        # start cycle
+        # 수강내용조회/출력 으로 이동
+        self._logic_move_to_sugang()
+
+        self._logic_cycle_01()
+        self._logic_cycle_01()
+        self._logic_cycle_01()
 
         return True
